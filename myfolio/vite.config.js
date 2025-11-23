fe4db924fb06dedb5fe8eeda9eb225cd3346f9d6
Kurt <151386675+kurt-navaja@users.noreply.github.com>
2025-11-24 00:20:40 +0800

    5th fix otw

--- myfolio/vite.config.js
+++ myfolio/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/kurt-navaja/Myfolio/tree/main/myfolio",
+  base: process.env.VITE_BASE_PATH || "/Myfolio/tree/main/myfolio",
 })
